897ede901798b8a50f52063e9e8989a4a61e9d10
Daniel Walls <walls.dk@gmail.com>
2023-07-27 18:35:43 -1000

    better victory objective

---
+++ Assets/Scripts/Entity Components/ShrinkOnInteract.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ShrinkOnInteract : InteractableEffect
+    {
+        [SerializeField] private float duration = 1;
+        
+        protected override void OnInteract(Interactable.InteractEvent eventData)
+        {
+            StartCoroutine(ShrinkCoroutine());
+        }
+
+        private IEnumerator ShrinkCoroutine()
+        {
+            float elapsed = 0;
+            Vector3 originalScale = transform.localScale;
+
+            while (elapsed < duration)
+            {
+                transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+            
+            transform.localScale = Vector3.zero;
+        }
+    }
+}
