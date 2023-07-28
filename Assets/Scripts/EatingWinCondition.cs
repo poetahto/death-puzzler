f12dfa7a01b3e24d6cb3fe7ad38588b126e2c259
Daniel Walls <walls.dk@gmail.com>
2023-07-27 18:59:13 -1000

    win-con changes

---
+++ Assets/Scripts/EatingWinCondition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class InteractAllWinCondition : MonoBehaviour
+    {
+        [SerializeField] private Interactable[] interactables;
+
+        public UnityEvent<Interactable.InteractEvent> onInteract;
+
+        private int _remaining;
+
+        private void Start()
+        {
+            _remaining = interactables.Length;
+
+            foreach (Interactable interactable in interactables)
+                interactable.onInteract.AddListener(HandleInteract);
+        }
+
+        private void HandleInteract(Interactable.InteractEvent eventData)
+        {
+            _remaining--;
+            onInteract.Invoke(eventData);
+
+            if (_remaining <= 0)
+                FindAnyObjectByType<GameplayStateMachine>().TransitionToVictory();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UICounter.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UICounter : MonoBehaviour
+    {
+        public TMP_Text text;
+        public int max;
+        public float scale;
+        public float restoreSpeed = 1;
+
+        private int _count;
+        private float _scale = 1;
+        private Vector3 _baseScale;
+
+        private void Start()
+        {
+            _baseScale = transform.localScale;
+        }
+
+        private void Update()
+        {
+            float t = 15 * Time.deltaTime;
+            transform.localScale = Vector3.Lerp(transform.localScale, _baseScale * _scale, t);
+            _scale = Mathf.MoveTowards(_scale, 1, restoreSpeed * Time.deltaTime);
+        }
+
+        public void Increment()
+        {
+            _count++;
+            _scale = scale;
+            text.text = $"{_count}/{max}";
+        }
+    }
+}
